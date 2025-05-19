5cf37446f8cc336616e60041c3ca6c6c539b32af
noah-m <noahmgss@gmail.com>
2025-05-18 23:14:31 -0700

    recopy

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747633912',resources: [
+    cacheName: 'chirpy-1747634767',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
